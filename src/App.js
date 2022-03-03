e631b36177825384a3472346af8e343aacd93c48
Noubar Topalian <topalian.noubar@gmail.com>
2022-03-03 13:10:28 +0100

    SignInSignUp pages, SignIn, CustomButton and FormInput Components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.compoent';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/signinsingup.component';
 import { Switch, Route } from 'react-router-dom'
 import './App.css';
 
@@ -12,6 +13,8 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
+
       </Switch>
     </div>
   );
