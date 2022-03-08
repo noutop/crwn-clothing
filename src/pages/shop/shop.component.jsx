06bb8cbed103e061fed42b73491b5b55890ee5ba
Noubar Topalian <topalian.noubar@gmail.com>
2022-03-08 14:22:17 +0100

    ShopData and irectory moved to redux reducer and created CollectionsOverview component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,26 +1,12 @@
 import React from 'react'
-import SHOP_DATA from './shop.data'
-import PreviewCollection from '../../components/preview-collection/preview-collection.component'
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
 
-class ShopPage extends React.Component{
-  constructor(props){
-    super(props)
+const ShopPage = ({collections}) => (
+  <div className='shop-page'>
+    <CollectionsOverview />
+  </div>
+)
 
-    this.state = {
-      collections: SHOP_DATA
-    }
-  }
 
-  render(){
-    const {collections} = this.state
-    return(
-      <div className='shop-page'>
-        {collections.map(({id, ...otherCollectionProps}) => (
-          <PreviewCollection key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    )
-  }
-}
 
 export default ShopPage
