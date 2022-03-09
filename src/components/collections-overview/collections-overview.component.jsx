59686237dc86fc10666a783ec6362d9f65694254
Noubar Topalian <topalian.noubar@gmail.com>
2022-03-09 14:08:38 +0100

    created respectives selectors and updated corresponding components

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import './collections-overview.styles.scss'
 import PreviewCollection from "../preview-collection/preview-collection.component";
-import {selectCollections} from '../../redux/shop/shop.selectors.js'
+import {selectCollectionsForPreview} from '../../redux/shop/shop.selectors.js'
 
 
 const CollectionsOverview = ({collections}) => (
@@ -15,7 +15,7 @@ const CollectionsOverview = ({collections}) => (
 )
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
+  collections: selectCollectionsForPreview
 })
 
 export default connect(mapStateToProps)(CollectionsOverview)
--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -5,10 +5,13 @@ import './collection.styles.scss'
 import CollectionItem from "../../components/collection-item/collection-item.component";
 
 const CollectionPage = ({collection}) => {
-  console.log(collection)
+  const {title, items} = collection
   return(
   <div className="collection-page">
-    <h2>Collection Page</h2>
+    <h2 className="title">{title}</h2>
+    <div className="items">
+      {items.map(item => <CollectionItem key={item.id} item={item} />)}
+    </div>
   </div>
 )}
 
